f80ee3d3f8135fdbdc66a64d34383cbe3443f703
Kirill <evilprog@yandex.ru>
2023-06-10 16:04:10 +0400

    stable version 0.0.1

--- app/src/main/java/com/example/seebuses/WebBrowser.java
+++ app/src/main/java/com/example/seebuses/WebBrowser.java
@@ -1,19 +1,25 @@
 package com.example.seebuses;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class WebBrowser extends AppCompatActivity {
     static String TransportURL;
-    private static WebView webView;
-    private static WebSettings settings;
+    private WebView webView;
+    private WebSettings settings;
+    private final Context context = WebBrowser.this;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -21,19 +27,41 @@ public class WebBrowser extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_browser);
         goWeb(TransportURL);
-
     }
 
     public void goBack(View view) {
         Intent goBack = new Intent(this, MainActivity.class);
         startActivity(goBack);
     }
+
     @SuppressLint("SetJavaScriptEnabled")
     public void goWeb(String TransportURL) {
         webView = findViewById(R.id.webBrowser);
         settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
+        settings.setGeolocationEnabled(true);
+        settings.setDomStorageEnabled(true);
+        webView.setNetworkAvailable(true);
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                Toast.makeText(context, "Загрузка", Toast.LENGTH_SHORT).show();
+            }
+
+            @SuppressLint("WebViewClientOnReceivedSslError")
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                Toast.makeText(context, "Загрузка завершена", Toast.LENGTH_SHORT).show();
+            }
+        });
         webView.loadUrl(TransportURL);
     }
 }
